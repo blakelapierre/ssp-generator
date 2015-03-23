5a7315c98cc92cf8fed35d4d6af8102081572910
Blake La Pierre <blakelapierre@gmail.com>
2015-03-23 03:08:42 -0700

    Initial commit

--- frontend/src/app.js
+++
@@ -1,6 +0,0 @@
-require('angular'); // Don't assign result...angular/browserify doesn't like it...
-
-module.exports = {
-  'ssp-generator': angular.module('ssp-generator', [])
-    .directive('generator',   require('./modules/ssp-generator/directives/generator/directive'))
-};
